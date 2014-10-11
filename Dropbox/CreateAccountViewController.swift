263d099ac1730e1a5639854c7508b2f51983c30d
atwoodamanda <atwooda86@gmail.com>
2014-10-11 11:30:32 -0700

    Manual segues complete.

---
+++ Dropbox/CreateAccountViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateAccountViewController.swift
+//  Dropbox
+//
+//  Created by Amanda Atwood on 10/8/14.
+//  Copyright (c) 2014 Amanda Atwood. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
